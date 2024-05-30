135d19c222d998b5b8ff37be4f4f12e76351dafb
KyleElAchkar <159497554+KyleElAchkar@users.noreply.github.com>
2024-05-30 18:56:34 -0400

    programmation du respawn terminer!!

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+   public float threshold;
+
+   void FixedUpdate()
+   {
+       if (transform.position.y < threshold)
+       {
+           transform.position = new Vector3(0f, 0f, 0f);
+       }
+       
+   }
+}
